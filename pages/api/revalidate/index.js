7f7b7ebbaa06089e8cb22eaab867e8a9cd8fdc97
Teguh Widodo <teguhwin8@gmail.com>
2022-08-12 19:38:21 +0700

    dinamyc revalidate

--- pages/api/revalidate/index.js
+++ pages/api/revalidate/index.js
@@ -3,8 +3,11 @@ export default async function handler(req, res) {
         return res.status(401).json({ message: "Invalid token" });
     }
 
+    const path = req.query.path;
+    const slug = req.quety.slug;
+
     try {
-        await res.revalidate("/");
+        await res.revalidate(`/${path ? path + (slug ? "/" + slug : "") : ""}`);
         return res.json({ revalidated: true });
     } catch (err) {
         return res.status(500).send("Error revalidating");
