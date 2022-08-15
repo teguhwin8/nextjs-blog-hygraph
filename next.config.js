2042a77de84359ba7194393d257d53875af5aa05
Teguh Widodo <teguhwin8@gmail.com>
2022-08-16 01:45:15 +0700

    Downgrade next-pws to 5.5.4

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-	pwa: {
-    dest: "public",
-		disable: process.env.NODE_ENV === 'development',
-    register: true,
-		skipWaiting: true
-  },
   reactStrictMode: true,
   images: {
     domains: ["media.graphassets.com"],
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    scope: '/',
+    skipWaiting: true
   },
 });
 
