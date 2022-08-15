61656784d3c1ab9284b27523cbf275b18f808282
Teguh Widodo <teguhwin8@gmail.com>
2022-08-16 00:00:45 +0700

    Add some information to sitemap xml

--- pages/sitemap.xml/index.js
+++ pages/sitemap.xml/index.js
@@ -1,13 +1,16 @@
 import { getServerSideSitemap } from "next-sitemap";
 import { getAllSlug } from "../../services";
+import moment from "moment";
 
 export async function getServerSideProps(ctx) {
   const data = await getAllSlug();
   const siteUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const fields = data.posts.map((post) => ({
     url: `${siteUrl}/posts/${post.slug}`,
+    loc: `${siteUrl}/posts/${post.slug}`,
     changefreq: "daily",
     priority: 0.5,
+    lastmod: moment(post.publishedAt).format("YYYY-MM-DD"),
   }));
   return getServerSideSitemap(ctx, fields);
 }
